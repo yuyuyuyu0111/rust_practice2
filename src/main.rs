99de0ad5704be1d209a3b1095cba45000a408124
yuyuyuyu0111 <yu.main.address2@gmail.com>
2024-07-03 21:24:32 +0900

    [じゃんけん]
    勝敗判定実装

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,25 @@ fn main() {
     loop {
         println!("次に出す手を入力してください");
         println!("1:グー 2:チョキ 3:パー 0:終了");
-        let a = input();
-        if a == 0 {
+        let player_hand = input();
+        if player_hand == 0 {
             break;
         }
 
-        let com_hand = index % 3+1;
+        let com_hand = index % 3 + 1;
 
-        println!("あなたの出した手は{}", hands(a));
+        println!("あなたの出した手は{}", hands(player_hand));
         println!("COMの出した手は{}", hands(com_hand));
 
-        if com_hand == a {
+        if com_hand == player_hand {
             println!("あいこ");
+        } else if (player_hand == 1 && com_hand == 2)
+            || (player_hand == 2 && com_hand == 3)
+            || (player_hand == 3 && com_hand == 1)
+        {
+            println!("かち！");
+        } else {
+            println!("まけ...");
         }
 
         if index == i64::MAX {
